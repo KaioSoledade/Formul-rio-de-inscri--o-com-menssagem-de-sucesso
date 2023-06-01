93a4478fa2bc8483026e6cb879b7f7321ade0e3b
Kaio Soledade <soledadekaio@gmail.com>
2023-06-01 18:07:07 -0300

    js para a impressão do gmail, sem resposta

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ function submit(){
     var n1 = document.querySelector('.email')
 
     document.querySelector('.recebe').innerHTML = n1.value
+
+    console.log = window.location = "sucesso.html"
 }
