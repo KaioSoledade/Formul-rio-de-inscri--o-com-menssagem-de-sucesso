fd109bb946334562d87820947599ca69798b97ba
Kaio Soledade <soledadekaio@gmail.com>
2023-06-07 12:16:31 -0300

    aba de sucesso completada, e email esta funcionand

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function submit(){
    document.querySelector('#sucesso').classList.add('show')
    document.querySelector('#sucesso').classList.remove('sucesso')
 
-   var n1 = document.querySelector('email').value
-   document.querySelector('.recebe').innerHTML = (n1)
+   var n1 = document.querySelector('.email')
+   document.querySelector('.recebe').innerHTML = n1.value
 }
 
 function vlt(){
