e9bef195235fc6e74063b7fdad9303704138ed53
Kaio Soledade <soledadekaio@gmail.com>
2023-06-07 11:59:01 -0300

    função 'show' add, falta innertext do email

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
-    const email = document.querySelector('.email');
-    localStorage.setItem("email", email.value);
-
 function submit(){
-   
+   document.querySelector('#sucesso').classList.add('show')
+   document.querySelector('#sucesso').classList.remove('sucesso')
+
+   var n1 = document.querySelector('email').value
+   document.querySelector('.recebe').innerHTML = (n1)
+}
+
+function vlt(){
+    document.querySelector('#sucesso').classList.add('sucesso')
+   document.querySelector('#sucesso').classList.remove('show')
+
 }
 
 
