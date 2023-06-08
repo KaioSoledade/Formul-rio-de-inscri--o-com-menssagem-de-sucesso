50401cac3a25d63b4fafd261c46b6fe593461f81
Kaio Soledade <soledadekaio@gmail.com>
2023-06-08 15:44:28 -0300

    fazendo 'if e else'

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ function submit(){
    document.querySelector('#sucesso').classList.add('show')
    document.querySelector('#sucesso').classList.remove('sucesso')
 
-   var n1 = document.querySelector('.email')
-   document.querySelector('.recebe').innerHTML = n1.value
+   if(){
+      var n1 = document.querySelector('.email')
+      document.querySelector('.recebe').innerHTML = n1.value
+   } else(){
+      document.querySelector('.label-email').innerHTML = 'Coloque um email valido!'
+   }
+   
 }
 
 function vlt(){
