e311c1322c230a36f54b157781e0ccef03081f1f
Kaio Soledade <soledadekaio@gmail.com>
2023-06-08 16:35:30 -0300

    o else ainda não funciona

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
 function submit(){
-   document.querySelector('#sucesso').classList.add('show')
-   document.querySelector('#sucesso').classList.remove('sucesso')
-
-   if(){
+   
+   var n2 = document.querySelector('.email')
+   if(n2){
+      document.querySelector('#sucesso').classList.add('show')
+      document.querySelector('#sucesso').classList.remove('sucesso')
       var n1 = document.querySelector('.email')
       document.querySelector('.recebe').innerHTML = n1.value
-   } else(){
+   } else{
       document.querySelector('.label-email').innerHTML = 'Coloque um email valido!'
    }
    
