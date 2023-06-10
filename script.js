f3a6747512baad75f7b6f03f30d748818aefa278
Kaio Soledade <soledadekaio@gmail.com>
2023-06-09 21:56:24 -0300

    tudo certo

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ function submit(){
       var n1 = document.querySelector('.email')
       document.querySelector('.recebe').innerHTML = n1.value
    } else{
-      document.querySelector('.label-email').innerHTML = 'Coloque um email valido!'
+      document.querySelector('#label').classList.add('label-email-2')
+      document.querySelector('#label').classList.remove('label-email')
+      document.querySelector('#label').innerHTML = 'Coloque um email valido!'
    }
    
 }
