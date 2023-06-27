6a61ee12be74fcb2352fe4b4c455b1782b5b0c57
Kaio Soledade <soledadekaio@gmail.com>
2023-06-27 11:05:47 -0300

    eliminação de arquivos

--- aula.js
+++
@@ -1,8 +0,0 @@
-var idade = 67
-if(idade < 16){
- console.log('não vota')
-} else if (idade <= 18|| idade >66){ 
-    console.log('voto opicional')
-} else if( idade > 18){
-    console.log('voto obrigatorio')
-}
--- sucesso.js
+++
@@ -1,9 +0,0 @@
-
-const name = localStorage.setItem('email');
-
-var n1 = document.querySelector('.receber')
-n1.innerHTML = 'name.value'
-
-function vlt(){
-    window.location = "index.html"
-}
